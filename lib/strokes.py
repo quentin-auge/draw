6bc43538208be9d81dc179c89c08836b45717aad
Quentin Augé <quentin.auge@gmail.com>
2019-03-10 18:10:00 +0100

    simplify strokes transformation code
    * Remove polar coordinates displacements transformation
    * Remove identity transformation
    * Remove Transformer class to keep 2 simple functions

--- lib/strokes.py
+++ lib/strokes.py
@@ -1,6 +1,3 @@
-import abc
-import math
-
 from lib.unpack import unpack_drawings
 
 
@@ -55,146 +52,75 @@ def flatten_strokes(strokes):
     return flat_strokes
 
 
-class StrokesTransformer(object):
+def transform_strokes(strokes):
     """
-    Transform a list of strokes to an intermediate representation, and back.
-    """
-
-    def transform(self, strokes):
-        """
-        Flatten and transform a list of strokes.
-        """
-        flat_strokes = flatten_strokes(strokes)
-        transformed_strokes = self._transform_flat_strokes(flat_strokes)
-        return transformed_strokes
-
-    def _transform_flat_strokes(self, flat_strokes):
-        """
-        Transform a list of flattened strokes to its intermediate representation.
-        Preserve stroke state.
-        """
-
-        transformed_strokes = []
-        shifted_flat_strokes = flat_strokes[1:] + [None]
-        for point, next_point in zip(flat_strokes, shifted_flat_strokes):
-            new_point = self._transform(point, next_point)
-            if new_point:
-                transformed_strokes.append(new_point)
-
-        # Mark end of drawing. That state might have been lost in transformation process.
-        v, w, *_ = transformed_strokes[-1]
-        transformed_strokes[-1] = [v, w, 0, 0, 1]
-
-        return transformed_strokes
-
-    @abc.abstractmethod
-    def _transform(self, point, next_point):
-        raise NotImplementedError
-
-    def inverse_transform(self, transformed_strokes, initial_point=(0, 0)):
-        """
-        Reconstruct a list of strokes from its intermediate representation using strokes state.
-
-        The first point may have been lost in the transformation process. It is
-        therefore possible to supply it to reconstruct the original strokes
-        without loosing information.
-        """
-
-        strokes = []
-        stroke_xs, stroke_ys = [], []
-
-        # Accumulator
-        acc = initial_point
-
-        for v, w, *stroke_state in transformed_strokes:
-
-            (x, y), acc = self._inverse_transform((v, w), acc)
-            stroke_xs.append(x)
-            stroke_ys.append(y)
-
-            _, is_end_of_stroke, is_end_of_drawing = stroke_state
-
-            if is_end_of_stroke:
-                # Start a new stroke
-                strokes.append([stroke_xs, stroke_ys])
-                stroke_xs, stroke_ys = [], []
-
-            if is_end_of_drawing:
-                break
-
-        # Flush accumulator
-        if acc is not None:
-            x, y = acc
-            stroke_xs.append(x)
-            stroke_ys.append(y)
-
-        # Flush last stroke
-        if stroke_xs and stroke_ys:
-            strokes.append((stroke_xs, stroke_ys))
-
-        return strokes
-
-    @abc.abstractmethod
-    def _inverse_transform(self, point, accumulator):
-        raise NotImplementedError
-
-
-class PointsStrokesTransformer(StrokesTransformer):
-    """
-    Transformation: identity.
+    First flatten strokes, then transform them from [(x, y)] points to [(dx, dy)] displacements
+    between two successive points in cartesian coordinates, while preserving stroke state.
     """
 
-    def _transform(self, point, _):
-        return point
+    flat_strokes = flatten_strokes(strokes)
 
-    def _inverse_transform(self, point, _):
-        return point, None
+    transformed_strokes = []
+    for point, next_point in zip(flat_strokes, flat_strokes[1:]):
+        x, y, *stroke_state = point
+        x_next, y_next, *_ = next_point
+        new_point = [x_next - x, y_next - y] + stroke_state
+        transformed_strokes.append(new_point)
 
+    # Mark end of drawing. That state might have been lost in transformation process.
+    dx, dy, *_ = transformed_strokes[-1]
+    transformed_strokes[-1] = [dx, dy, 0, 0, 1]
 
-class VectorsStrokesTransformer(StrokesTransformer):
-    def _transform(self, point, next_point):
-        """
-        Transformation: two-points -> cartesian-coordinates vector.
-        """
-        if next_point is not None:
-            x, y, *stroke_state = point
-            x_next, y_next, *_ = next_point
-            new_point = [x_next - x, y_next - y] + stroke_state
-            return new_point
-
-    def _inverse_transform(self, point, acc_point):
-        new_point = acc_point
-
-        delta_x, delta_y = point
-        x, y = acc_point
-        acc_point = (x + delta_x, y + delta_y)
-
-        return new_point, acc_point
+    return transformed_strokes
 
 
-class PolarStrokesTransformer(StrokesTransformer):
-    def _transform(self, point, next_point):
-        """
-        Transformation: two-points -> polar-coordinates vector.
-        """
-        if next_point is not None:
-            x, y, *stroke_state = point
-            x_next, y_next, *_ = next_point
-            distance = math.sqrt((x_next - x) ** 2 + (y_next - y) ** 2)
-            angle = math.atan2(y_next - y, x_next - x)
-            new_point = [distance, angle] + stroke_state
-            return new_point
+def inverse_transform_strokes(transformed_strokes):
+    """
+    First untransform transformed strokes, then unflatten them.
 
-    def _inverse_transform(self, point, acc_point):
-        new_point = acc_point
+    The first point has been lost in the transformation process. It is therefore
+    not possible to reconstruct the exact original strokes. The reconstructed strokes
+    will thus be a translation of the original strokes.
+    """
 
-        distance, angle = point
-        x, y = acc_point
-        x = int(x + distance * math.cos(angle))
-        y = int(y + distance * math.sin(angle))
-        acc_point = (x, y)
+    strokes = []
+    stroke_xs, stroke_ys = [0], [0]
+    x, y = 0, 0
 
-        return new_point, acc_point
+    for dx, dy, *stroke_state in transformed_strokes:
+
+        _, is_end_of_stroke, is_end_of_drawing = stroke_state
+
+        if is_end_of_stroke:
+            # Start a new stroke
+            strokes.append([stroke_xs, stroke_ys])
+            stroke_xs, stroke_ys = [], []
+
+        x += dx
+        y += dy
+
+        stroke_xs.append(x)
+        stroke_ys.append(y)
+
+        if is_end_of_drawing:
+            break
+
+    # Flush last stroke
+    if stroke_xs and stroke_ys:
+        strokes.append((stroke_xs, stroke_ys))
+
+    return strokes
+
+
+def get_n_points(strokes):
+    """
+    Get number of points in a drawing.
+    """
+
+    n_points = 0
+    for x, y in strokes:
+        n_points += len(x)
+    return n_points
 
 
 def cut_strokes(strokes, n_points):
@@ -214,38 +140,16 @@ def cut_strokes(strokes, n_points):
     return result_strokes
 
 
-def get_n_points(strokes):
-    """
-    Get number of points in a drawing.
-    """
-
-    n_points = 0
-    for x, y in strokes:
-        n_points += len(x)
-    return n_points
-
-
 if __name__ == '__main__':
     from pprint import pprint
-    from matplotlib import pyplot as plt
-    from lib.plot import plot
 
     dataset = unpack_drawings('./data/The Eiffel Tower.bin')
     strokes = next(dataset)['image']
 
     pprint(strokes)
 
-    transformer = PointsStrokesTransformer()
-    transformer = VectorsStrokesTransformer()
-    transformer = PolarStrokesTransformer()
-
-    transformed_strokes = transformer.transform(strokes)
+    transformed_strokes = transform_strokes(strokes)
     pprint(transformed_strokes)
 
-    reconstructed_strokes = transformer.inverse_transform(transformed_strokes,
-                                                          initial_point=(0, 218))
+    reconstructed_strokes = inverse_transform_strokes(transformed_strokes)
     pprint(reconstructed_strokes)
-
-    plot(strokes)
-    plot(reconstructed_strokes)
-    plt.show()
